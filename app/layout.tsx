6db3702158d686215582f49acf26c5c716d59764
Alex Lamper <lamp0020@hz.nl>
2024-09-27 19:23:15 +0200

    Updated Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider"
-import {
-  ClerkProvider,
-} from '@clerk/nextjs'
+import { ClerkProvider } from '@clerk/nextjs'
 import Navbar from "@/components/common/Navbar";
 import Footer from "@/components/common/Footer";
 
@@ -18,9 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider
-      publishableKey={process.env.NEXT_PUBLIC_CLERK_API_KEY}
-    >
+    <ClerkProvider>
       <html lang="en">
         <head>
           <link
--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -1,54 +1,45 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { ModeToggle } from '../mode-toggle';
 import { 
+    ClerkLoaded, 
+    ClerkLoading,
     SignedIn,
     SignedOut,
     SignInButton,
-    // UserButton,
-    SignOutButton,
-    ClerkLoading,
-    SignUpButton,
+    UserButton,
 } from '@clerk/nextjs';
 import { Loader } from 'lucide-react';
-import Image from 'next/image';
-import Link from 'next/link';
 import { Button } from '../ui/button';
-import { ModeToggle } from '../mode-toggle';
 
 const Navbar = () => {
     return (
-        <nav className="border-b p-4">
-            <div className="container mx-auto flex justify-between items-center">
-                {/* Logo */}
-                <Link href="/">
-                    <Image src={'/logo/logo.svg'} alt={'logo'} width={200} height={200} />
-                </Link>
+        <nav className="border-b h-20 w-full px-4">
+            <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full">
+                <div className='pt-8 pl-4 pb-7 flex items-center gap-x-3'>
+                    {/* Logo */}
+                    <Link href="/">
+                        <Image src={'/logo/logo.svg'} alt={'logo'} width={200} height={200} />
+                    </Link>
+                </div>
 
-                {/* Links and User Options */}
-                <div className="flex space-x-4">
-                    <ClerkLoading>
-                        <Loader className='h-5 w-5 text-muted-foreground animate-spin' />
-                    </ClerkLoading>
+                <ClerkLoading>
+                    <Loader className='h-5 w-5 text-muted-foreground animate-spin' />
+                </ClerkLoading>
+                <ClerkLoaded>
                     <SignedIn>
-                        <Link href="/quizzes">
-                            <p className="font-roboto">Quizzes</p>
-                        </Link>
-                        <Link href="/profile">
-                            <p className="font-roboto">Profile</p>
-                        </Link>
-                        {/* <UserButton /> */}
-                        <SignOutButton>
-                            <Button>Sign Out</Button>
-                        </SignOutButton>
+                        <UserButton />
                     </SignedIn>
                     <SignedOut>
-                        <SignInButton>
+                        <SignInButton
+                            mode='modal'
+                        >
                             <Button>Login</Button>
                         </SignInButton>
-                        <SignUpButton>
-                            <Button>Sign Up</Button>
-                        </SignUpButton>
                     </SignedOut>
-                    <ModeToggle />
-                </div>
+                </ClerkLoaded>
+
+                <ModeToggle />
             </div>
         </nav>
     );
