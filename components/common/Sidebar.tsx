7fd5588be57a4e4de2722d648ba74516ba37f980
Alex <lamp0020@hz.nl>
2024-10-03 20:46:37 +0200

    Added Todo List + Updated Sidebar

--- components/common/Sidebar.tsx
+++ components/common/Sidebar.tsx
@@ -2,43 +2,45 @@
 
 import Link from 'next/link';
 import { useState } from 'react';
+import { usePathname } from 'next/navigation';
 import { MdHome, MdInfo } from 'react-icons/md';
 import { FaGlobe, FaQuestionCircle, FaUser } from 'react-icons/fa';
 
 const Sidebar = () => {
   const [isOpen] = useState(false);
+  const pathname = usePathname();
 
   return (
-    <div className={`fixed inset-y-0 left-0 z-50 w-64 border-r bg-black bg-opacity-5 transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-300 ease-in-out md:relative md:translate-x-0 h-auto`}> {/* Set h-auto for height */}
-      <nav className="p-8 h-full"> {/* Added h-full to ensure full height */}
+    <div className={`fixed inset-y-0 left-0 z-50 w-64 border-r bg-black bg-opacity-5 transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-300 ease-in-out md:relative md:translate-x-0 h-auto`}>
+      <nav className="p-8 h-full">
         <ul>
-          <li className="mb-4 hover:bg-white hover:border hover:rounded-[0.4rem] flex items-center">
-            <MdHome size={23} color="black" className="mr-2 ml-4" />
-            <Link href="/" className="block px-4 py-2 rounded font-medium">
+          <li className={`mb-4 flex items-center hover:bg-opacity-70 hover:border hover:rounded-[0.4rem] cursor-pointer ${pathname === '/' || pathname === '/dashboard' ? 'bg-[#ffffff1a] bg-opacity-50 border rounded-[0.4rem]' : ''}`}>
+            <Link href="/" className="flex items-center w-full px-4 py-2 rounded font-medium">
+              <MdHome size={23} className="mr-2" />
               Dashboard
             </Link>
           </li>
-          <li className="mb-4 hover:bg-white hover:border hover:rounded-[0.4rem] flex items-center">
-            <FaGlobe size={20} color="black" className="mr-2 ml-4" />
-            <Link href="/quizzes" className="block px-4 py-2 rounded font-medium">
+          <li className={`mb-4 flex items-center hover:bg-white hover:border hover:rounded-[0.4rem] cursor-pointer ${pathname === '/quizzes' ? 'bg-white border rounded-[0.4rem]' : ''}`}>
+            <Link href="/quizzes" className="flex items-center w-full px-4 py-2 rounded font-medium">
+              <FaGlobe size={20} className="mr-2" />
               Quizzes
             </Link>
           </li>
-          <li className="mb-4 hover:bg-white hover:border hover:rounded-[0.4rem] flex items-center">
-            <FaUser size={20} color="black" className="mr-2 ml-4" />
-            <Link href="/profile" className="block px-4 py-2 rounded font-medium">
+          <li className={`mb-4 flex items-center hover:bg-white hover:border hover:rounded-[0.4rem] cursor-pointer ${pathname === '/profile' ? 'bg-white border rounded-[0.4rem]' : ''}`}>
+            <Link href="/profile" className="flex items-center w-full px-4 py-2 rounded font-medium">
+              <FaUser size={20} className="mr-2" />
               Profile
             </Link>
           </li>
-          <li className="mb-4 hover:bg-white hover:border hover:rounded-[0.4rem] flex items-center">
-            <MdInfo size={20} color="black" className="mr-2 ml-4" />
-            <Link href="/about" className="block px-4 py-2 rounded font-medium">
+          <li className={`mb-4 flex items-center hover:bg-white hover:border hover:rounded-[0.4rem] cursor-pointer ${pathname === '/about' ? 'bg-white border rounded-[0.4rem]' : ''}`}>
+            <Link href="/about" className="flex items-center w-full px-4 py-2 rounded font-medium">
+              <MdInfo size={20} className="mr-2" />
               About
             </Link>
           </li>
-          <li className="mb-4 hover:bg-white hover:border hover:rounded-[0.4rem] flex items-center">
-            <FaQuestionCircle size={20} color="black" className="mr-2 ml-4" />
-            <Link href="/help" className="block px-4 py-2 rounded font-medium">
+          <li className={`mb-4 flex items-center hover:bg-white hover:border hover:rounded-[0.4rem] cursor-pointer ${pathname === '/help' ? 'bg-white border rounded-[0.4rem]' : ''}`}>
+            <Link href="/help" className="flex items-center w-full px-4 py-2 rounded font-medium">
+              <FaQuestionCircle size={20} className="mr-2" />
               Help
             </Link>
           </li>
