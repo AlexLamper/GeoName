5124a02734bdd63fa6cdd51d4e8b095c456966e1
Alex <lamp0020@hz.nl>
2024-10-23 15:20:09 +0200

    Updated Refresh State Leaderboard

--- app/leaderboard/page.tsx
+++ app/leaderboard/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
+import React, { useState } from 'react';
 import BackButton from '@/components/buttons/BackButton';
 import Sidebar from '@/components/common/Sidebar';
 import Leaderboard from '@/components/Leaderboard';
 
 const LeaderboardPage = () => {
+  const [refreshKey] = useState(0);
 
   return (
     <div className="flex min-h-screen">
@@ -12,7 +14,7 @@ const LeaderboardPage = () => {
       <Sidebar />
       <main className="flex-1 p-6 lg:max-w-[80%] max-w-full">
         <BackButton />
-        <Leaderboard />
+        <Leaderboard key={refreshKey} />
       </main>
     </div>
   );
