1ddced5df942f1386f41621ad1baaad4989fd221
Alex <lamp0020@hz.nl>
2024-10-31 20:53:10 +0100

    Added Page Reload Leaderboard Page

--- app/leaderboard/page.tsx
+++ app/leaderboard/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import BackButton from '@/components/buttons/BackButton';
 import Sidebar from '@/components/common/Sidebar';
 import Leaderboard from '@/components/Leaderboard';
@@ -8,6 +8,14 @@ import Leaderboard from '@/components/Leaderboard';
 const LeaderboardPage = () => {
   const [refreshKey] = useState(0);
 
+  useEffect(() => {
+    const hasReloaded = sessionStorage.getItem('reloaded');
+    if (!hasReloaded) {
+      sessionStorage.setItem('reloaded', 'true');
+      window.location.reload();
+    }
+  }, []);
+  
   return (
     <div className="flex min-h-screen">
       {/* Sidebar */}
