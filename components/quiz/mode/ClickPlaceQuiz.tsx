06a2c08446059b1e3acd1a86aa98c0c7aa1e1ad7
Alex <lamp0020@hz.nl>
2024-10-31 20:37:06 +0100

    Updated Correct + Incorrect Message Popup

--- components/quiz/mode/ClickPlaceQuiz.tsx
+++ components/quiz/mode/ClickPlaceQuiz.tsx
@@ -18,7 +18,7 @@ const ClickPlaceQuiz: React.FC<ClickPlaceQuizProps> = ({ places }) => {
   const [score, setScore] = useState<number>(0);
   const [message, setMessage] = useState<string>('');
   const [messageCorrect, setCorrectMessage] = useState<string>('');
-  const [showMessage, setShowMessage] = useState<boolean>(false); // New state for message visibility
+  const [showMessage, setShowMessage] = useState<boolean>(false); // State for message visibility
   const [quizPlaces, setQuizPlaces] = useState<Place[]>([]);
 
   useEffect(() => {
@@ -33,11 +33,10 @@ const ClickPlaceQuiz: React.FC<ClickPlaceQuizProps> = ({ places }) => {
         .sort(() => 0.5 - Math.random())
         .slice(0, 4); // Select 4 random places (excluding the current one)
 
-      // Include the correct place in the set of places to display
       const newQuizPlaces = [randomPlace, ...randomSelection].sort(() => 0.5 - Math.random());
       setCurrentPlace(randomPlace);
       setQuizPlaces(newQuizPlaces);
-      setMessage(`Click on ${randomPlace.name}`); // Update message with randomPlace
+      setMessage(`Click on ${randomPlace.name}`);
     }
   };
 
@@ -45,23 +44,15 @@ const ClickPlaceQuiz: React.FC<ClickPlaceQuizProps> = ({ places }) => {
     if (currentPlace && placeId === currentPlace.id) {
       setScore(score + 1);
       setCorrectMessage('correct');
-      setShowMessage(true); // Show message
-      setTimeout(() => {
-        setShowMessage(false); // Hide after 1 second
-        generateNewPlaces();
-      }, 1000); // 1 second delay for showing message
     } else {
       setCorrectMessage('incorrect');
-      setShowMessage(true); // Show message
-      setTimeout(() => {
-        setShowMessage(false); // Hide after 1 second
-      }, 1000); // 1 second delay for showing message
     }
+    setShowMessage(true);
+    setTimeout(() => setShowMessage(false), 1000); // Hide after 1 second
   };
 
   return (
     <div>
-      {/* CSS for the fadeIn and fadeOut animations */}
       <style>
         {`
           @keyframes fadeIn {
@@ -84,9 +75,29 @@ const ClickPlaceQuiz: React.FC<ClickPlaceQuizProps> = ({ places }) => {
         `}
       </style>
 
-      <div className='bg-black bg-opacity-5 dark:bg-white dark:bg-opacity-5 p-4 rounded-[0.3rem] border'>
+      {/* Popup Message */}
+      {showMessage && (
+        <div
+          className={`fixed top-20 left-1/2 transform -translate-x-1/2 z-50 ${
+            showMessage ? 'fade-in' : 'fade-out'
+          }`}
+        >
+          {messageCorrect === 'correct' ? (
+            <h3 className="flex items-center text-lg mb-4 text-green-700 bg-green-100 border border-green-500 border-opacity-20 rounded-[0.4rem] py-2 px-8 shadow-md font-bold max-w-[100%] mt-2">
+              <IoIosCheckmarkCircle className="mr-2" />
+              Correct!
+            </h3>
+          ) : (
+            <h3 className="flex items-center text-lg mb-4 text-red-700 bg-red-100 border border-red-500 border-opacity-20 rounded-[0.4rem] py-2 px-8 shadow-md font-bold max-w-[100%] mt-2">
+              <HiXCircle className="mr-2" />
+              Try Again!
+            </h3>
+          )}
+        </div>
+      )}
+
+      <div className="bg-black bg-opacity-5 dark:bg-white dark:bg-opacity-5 p-4 rounded-[0.3rem] border">
         <h2 className="text-2xl font-bold">
-          {/* Display the message with styled place name */}
           {message.split(' ').map((word, index) =>
             word === currentPlace?.name ? (
               <span key={index} style={{ color: '#1A5319', textDecoration: 'underline' }}>{word}</span>
@@ -96,30 +107,6 @@ const ClickPlaceQuiz: React.FC<ClickPlaceQuizProps> = ({ places }) => {
           )}
         </h2>
       </div>
-
-      {/* Correct Message */}
-      {messageCorrect === 'correct' && showMessage && (
-        <h3
-          className={`flex items-center text-lg mb-4 text-green-700 bg-green-100 border border-green-500 border-opacity-20 rounded-[0.4rem] p-3 shadow-md font-bold lg:max-w-[40%] max-w-[90%] mt-2 ${
-            showMessage ? 'fade-in' : 'fade-out'
-          }`}
-        >
-          <IoIosCheckmarkCircle className="mr-2" />
-          Correct!
-        </h3>
-      )}
-
-      {/* Incorrect Message */}
-      {messageCorrect === 'incorrect' && showMessage && (
-        <h3
-          className={`flex items-center text-lg mb-4 text-red-700 bg-red-100 border border-red-500 border-opacity-20 rounded-[0.4rem] p-3 shadow-md font-bold lg:max-w-[40%] max-w-[90%] mt-2 ${
-            showMessage ? 'fade-in' : 'fade-out'
-          }`}
-        >
-          <HiXCircle className="mr-2" />
-          Try Again!
-        </h3>
-      )}
 
       <QuizMap
         center={currentPlace ? currentPlace.position : [0, 0]}
